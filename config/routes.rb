f8386ce6df96cbc0fd970f61b4a1cdb312bb0a6c
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-04 09:03:30 +0000

    [Add] コントローラーとビューの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :admins,controllers: {
     path: "/admin",
     registrations: 'admin/admins/registrations',
@@ -7,9 +8,33 @@ Rails.application.routes.draw do
   }
 
   devise_for :users, controllers: {
-    registraions: 'public/users/registrations',
     sessions: 'public/users/sessions',
-    passwords: 'public/users/passwords'
+    passwords: 'public/users/passwords',
+    registrations: 'public/users/registrations'
   }
+
+  scope module: :admin do
+    root to: 'homes#top'
+    get 'searches', to: 'searches#search'
+    get 'searches', to: 'searches#index'
+    resources :users, except: [:create, :new, :destroy]
+    resources :genres
+    resources :recipes, only: [:index, :show, :destroy]
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about', to: 'homes#about'
+    resources :bookmarks, only: [:create, :destroy]
+    resources :recipes, only: [:create, :destroy]
+    get 'confirm', to: 'recipes#confirm'
+    resources :reviews, except: [:show, :edit, :update]
+    resources :users, only: [:show, :edit, :update]
+    get 'users/cancel', to: 'searches#cancel'
+    get 'users/unsubscribe', to: 'searches#unsubscribe'
+    get 'recipes', to: 'searches#index'
+    get 'searches', to: 'searches#search'
+    get 'rankings', to: 'rankings#index'
+  end
 
 end
