f84536c553c8d6395749c939a4014165d73f85a4
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-04 03:56:56 +0000

    [Add] テーブルの設定

--- db/migrate/20210804013616_create_genres.rb
+++ db/migrate/20210804013616_create_genres.rb
@@ -1,7 +1,11 @@
 class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
-
+      t.string   :name,           null: false
+      t.string   :genre_image_id, null: false
+      t.datetime :created_at,     null: false
+      t.datetime :updated_at,     null: false
+      
       t.timestamps
     end
   end
