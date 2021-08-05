1e488c5f5046795a94eb5f1b059ee75a59d26cf8
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-05 15:48:23 +0000

    [Add] レシピのフォーム・コントローラーの調整

--- db/migrate/20210804013826_create_procedures.rb
+++ db/migrate/20210804013826_create_procedures.rb
@@ -2,7 +2,6 @@ class CreateProcedures < ActiveRecord::Migration[5.2]
   def change
     create_table :procedures do |t|
       t.integer  :recipe_id,          null: false
-      t.string   :procedure_image_id
       t.text     :explanation,        null: false
       t.datetime :created_at,         null: false
       t.datetime :updated_at,         null: false
