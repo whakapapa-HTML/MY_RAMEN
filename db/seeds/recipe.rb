31c26af2af19954e6d4a1f89b41a5634e2b421ec
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-08 09:14:33 +0000

    [Add] レシピ投稿 アップデート　削除

--- db/seeds/recipe.rb
+++ db/seeds/recipe.rb
@@ -1,14 +1,14 @@
 Recipe.create!([
   {user_id: 1, genre_id: 1, name: "煮干しが効いてる醤油ラーメン", introduction: "美味しく簡単に作れます！！スーパーの食材だけで簡単に作ることができます！！手軽に簡単調理でお店の味！！", serving: 4, recipe_image: open("#{Rails.root}/db/fixtures/DSCF9743-removebg-preview.png",
   ingredients_attributes:[
-    { recipe_id: 1, name: "煮干し", amount: 300},
-    { recipe_id: 1, name: "椎茸", amount: 100},
-    { recipe_id: 1, name: "昆布", amount: 100},
-    { recipe_id: 1, name: "肩ロース", amount: 400},
-    { recipe_id: 1, name: "めんま", amount: 100},
-    { recipe_id: 1, name: "鶏ガラ", amount: 300},
-    { recipe_id: 1, name: "ゲンコツ", amount: 500},
-    { recipe_id: 1, name: "醤油", amount: 100}
+    { recipe_id: 1, name: "煮干し", amount: 300, per_amount: 75},
+    { recipe_id: 1, name: "椎茸", amount: 100, per_amount: 25},
+    { recipe_id: 1, name: "昆布", amount: 100, per_amount: 25},
+    { recipe_id: 1, name: "肩ロース", amount: 400, per_amount: 100},
+    { recipe_id: 1, name: "めんま", amount: 100, per_amount: 25},
+    { recipe_id: 1, name: "鶏ガラ", amount: 300, per_amount: 75},
+    { recipe_id: 1, name: "ゲンコツ", amount: 500,per_amount: 125},
+    { recipe_id: 1, name: "醤油", amount: 100, per_amount: 25}
   ],
   procedures_attributes:[
     { recipe_id: 1, explanation: "鶏ガラを下茹でする", recipe_image: open("#{Rails.root}/db/fixtures/DSCF9743-removebg-preview.png")},
