6591984eec0958d8d922d1a538f1076ce16f703d
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-10 13:37:23 +0000

    [Edit] jsファイルをapplication.jsへ移行

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,9 +23,33 @@
 
 /*global $*/
 
+ $(function() {
+
+      function readURL(input) {
+        if (input.files && input.files[0]) {
+          var reader = new FileReader();
+          reader.onload = function (e) {
+            $('#img_prev').attr('src', e.target.result);
+          };
+          reader.readAsDataURL(input.files[0]);
+        }
+      }
+      $("#procedure_img").change(function() {
+        readURL(this);
+    });
+ });
 
 
 
 // document.addEventListener("turbolinks:load", function () { //turbolinks:loadによって、リロードを解決
 
 
+$(document).ready(function(){
+    var tabs = $(".tab"); // tabのクラスを全て取得し、変数tabsに配列で定義
+    $(".tab").on("click", function() { // tabをクリックしたらイベント発火
+      $(".active").removeClass("active"); // activeクラスを消す
+      $(this).addClass("active"); // クリックした箇所にactiveクラスを追加
+      const index = tabs.index(this); // クリックした箇所がタブの何番目か判定し、定数indexとして定義
+      $(".content").removeClass("show").eq(index).addClass("show"); // showクラスを消して、contentクラスのindex番目にshowクラスを追加
+    });
+  });
