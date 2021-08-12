e36a1dcd39cd2b286fc6e0cd730b95d55ef76604
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-12 09:34:32 +0000

    [Add] ログインユーザーの権限付与

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   #protectedは呼び出された他のコントローラからも参照可能
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
+
+  def authenticate
+    return if user_signed_in?
+    redirect_to new_user_session_path, alert: 'ログインしてください'
+  end
+
 end
