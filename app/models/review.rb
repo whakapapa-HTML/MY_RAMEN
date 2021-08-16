3e0bd2994f9a5b5cc81adee02cc00a238fcfc4bd
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-16 06:14:19 +0000

    [Update] レビューの表示調整

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,4 +8,10 @@ class Review < ApplicationRecord
     validates :body
     validates :evaluation
   end
+  
+  validates :evaluation, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }
+  
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  mail = ENV['MAILER_USER_ID']
+  pass = ENV['MAILER_PASSWORD']
+
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            mail,
+    password:             pass,
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+  
 end
