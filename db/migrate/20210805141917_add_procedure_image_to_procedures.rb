432c752809c02bdaff498349d53592940a3bc0b4
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-16 15:38:14 +0000

    [Add] migrationを一括

--- db/migrate/20210805141917_add_procedure_image_to_procedures.rb
+++
@@ -1,5 +0,0 @@
-class AddProcedureImageToProcedures < ActiveRecord::Migration[5.2]
-  def change
-    add_column :procedures, :procedure_image, :string
-  end
-end
--- db/migrate/20210812143118_change_column_to_genre.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToGenre < ActiveRecord::Migration[5.2]
-  def change
-    change_column :genres, :genre_image, :string, null: false
-  end
-end
