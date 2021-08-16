8e061c3f3d1476118b96bfbf55207b050dd827a0
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-16 08:37:22 +0000

    [Add] ユーザーのログイン権限の設定

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module MYRamen
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
