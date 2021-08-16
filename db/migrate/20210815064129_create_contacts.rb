220f2d443c0d5a18745c83559b4759bdc3e4ec15
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-08-16 11:10:14 +0000

    [Add] UntrackedFile

---
+++ db/migrate/20210815064129_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :contacts do |t|
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.text :body, null: false
+      t.text :reply
+      t.integer :status, default: 0, null: false
+      t.timestamps
+    end
+  end
+end
